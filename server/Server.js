326bfc7a7e27ea788da2c4893f4830876a6856a1
Katja Kontio <katja@gopeach.se>
2023-06-27 10:52:29 +0200

    server config

--- server/Server.js
+++ server/Server.js
@@ -1,45 +1,24 @@
 import express from 'express';
-import dotenv from 'dotenv';
-import helmet from 'helmet'
-import morgan from 'morgan'
+import helmet from 'helmet';
+import morgan from 'morgan';
+import middlewares from './src/middlewares/MiddleWares.js';
+import Config from './config/Config.js';
 
 dotenv.config()
 const app = express();
+
 app.use(helmet());
 app.use(morgan('common'));
-// app.use(isAuthenticated)
-const port = process.env.PORT
+
 
 app.get("/exercises", (req, res) =>  {
     res.send("Stretcha bröstmuskel")
 })
 
-app.get("/user", isAuthenticated, (req, res) =>  {
-})
 
-app.use(notFound)
+app.use(middlewares.notFound)
+app.use(middlewares.errorHandler);
 
-function isAuthenticated(req, res, next) {
-    req.query.admin === 'true'
-    ? res.send("You are admin")
-    : res.send("You cannot make calls to this API URL")
-    next()
-}
+Config.connectToDatabase();
+Config.connectToPort(app);
 
-function notFound(req, res, next) {
-    const error = new Error (`Not found!: ${req.originalUrl}`)
-    res.status(404)
-    next(error)
-}
-
-function errorHandler(error, req, res, next) {
-    const statusCode = res.statuscode === 200 ? 500: res.statuscode
-    res.status(statusCode)
-    res.json({
-        statuscode: statuscode
-    })
-}
-
-app.listen(port,  () => {
-    console.log(`Server är igång på port ${port}`);
-})
---
+++ server/config/Config.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const connectToDatabase = async () => {
+    try {
+        const DB_URL = process.env.DATABASE_URL
+        await mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true })
+            console.log('SUCESSFULLY CONNECTED TOT THE DATABASE!')
+    } catch (error) {
+        console.log('ERROR WHILE TRYING TOT CONNECT TO THE DATABASE: ', error)
+    process.exit()
+}
+}
+
+const connectToPort = (app) => {
+    const port = process.env.PORT
+    app.listen(port,  () => {
+        console.log(`Server är igång på port ${port}`);
+    })
+}
+
+export default {
+    connectToDatabase,
+    connectToPort
+}
