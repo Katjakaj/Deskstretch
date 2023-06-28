fabadcd512799fea93e42c50857be5fa255bf08b
Katja Kontio <katja@gopeach.se>
2023-06-28 15:46:11 +0200

    added controllers, models, routes

--- server/Server.js
+++ server/Server.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import bodyParser from 'body-parser';
 import middlewares from './src/middlewares/MiddleWares.js';
+import UserRoutes from './src/routes/User.routes.js';
 import Config from './config/Config.js';
+import dotenv from 'dotenv';
 
 dotenv.config()
+
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 app.use(helmet());
 app.use(morgan('common'));
 
@@ -15,7 +21,7 @@ app.get("/exercises", (req, res) =>  {
     res.send("Stretcha bröstmuskel")
 })
 
-
+UserRoutes.routes(app)
 app.use(middlewares.notFound)
 app.use(middlewares.errorHandler);
 
