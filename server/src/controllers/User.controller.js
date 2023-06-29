e5307bc97bae75e03521cf7816cc3b7e76207903
Katja Kontio <katja@gopeach.se>
2023-06-29 15:37:29 +0200

    database setup

--- server/src/controllers/User.controller.js
+++ server/src/controllers/User.controller.js
@@ -15,6 +15,44 @@ const createUser = async (req, res) => {
     }
 }
 
+const getAllUsers = async (req, res) => {
+    try {
+    const response = await UserModel.find()
+    res.status(200).send(response)
+    } catch(error) {
+        res.status(500).send({meesage: error.message})
+    }
+}
+
+const getUserWithId = async (req, res) => {
+    try {
+        const response = await UserModel.findById(req.params.userId)
+        res.status(200).send(response)
+    } catch (error) {
+        res.status(500).send({meesage: "Error occured whie trying to retrive user with ID:  " + req.params.userId,
+        error: error.message
+    })
+    }
+}
+
+const getUserWithUsernameQuery = async (req, res) => {
+    try {
+        const response = await UserModel.find({username: req.query.username })
+        response.length !== 0 
+        ? res.status(200).send(response) 
+        : res.status(404).send({message: 'Could not find user with username: ' + req.query.username }) 
+    } catch (error) {
+        res.status(500).send({
+            message: 'Error occured while tring to retrice user with username: '  + req.query.puserId,
+            error: error.message
+        })
+
+    }
+}
+
 export default {
-    createUser
+    createUser,
+    getAllUsers,
+    getUserWithId,
+    getUserWithUsernameQuery
 }
--- server/src/routes/User.routes.js
+++ server/src/routes/User.routes.js
@@ -2,6 +2,9 @@ import UserController from "../controllers/User.controller.js";
 
 const routes = (app) => {
     app.post('/user', UserController.createUser)
+    app.get('/user', UserController.getAllUsers)
+    app.get('/user/:userId', UserController.getUserWithId)
+    app.get('/searchuser', UserController.getUserWithUsernameQuery)
 }
 
 export default {
